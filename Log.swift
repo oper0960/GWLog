59fc631049288aa0f047958ef2205eb61469b7d3
Felix <felix@hpcnt.com>
2017-10-10 10:03:07 +0900

    Optimize code.

--- Log.swift
+++ Log.swift
@@ -5,38 +5,50 @@
 //  Copyright © 2017년 Ryugilwan. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class Log {
-    
-    enum LogType: String {
+public class Log {
+
+    // MARK: - Enumerable
+
+    public enum LogType: String {
         case info = "[INFO]"
         case debug = "[DEBUG]"
         case warning = "[WARNING]"
         case error = "[ERROR]"
         case emergency = "[EMERGENCY]"
     }
+
+    // MARK: - Property
+
+    static var fileName: String = "log.txt"
+
+
+    // MARK: - Public
     
-    class func info(file: String = #file, function: String = #function, line: Int = #line, message: String) {
+    public class func info(file: String = #file, function: String = #function, line: Int = #line, message: String) {
         appendLog(type: .info, file: file, function: function, line: line, message: message)
     }
     
-    class func debug(file: String = #file, function: String = #function, line: Int = #line, message: String) {
+    public class func debug(file: String = #file, function: String = #function, line: Int = #line, message: String) {
         appendLog(type: .debug, file: file, function: function, line: line, message: message)
     }
     
-    class func warning(file: String = #file, function: String = #function, line: Int = #line, message: String) {
+    public class func warning(file: String = #file, function: String = #function, line: Int = #line, message: String) {
         appendLog(type: .warning, file: file, function: function, line: line, message: message)
     }
     
-    class func error(file: String = #file, function: String = #function, line: Int = #line, message: String) {
+    public class func error(file: String = #file, function: String = #function, line: Int = #line, message: String) {
         appendLog(type: .error, file: file, function: function, line: line, message: message)
     }
     
-    class func emergency(file: String = #file, function: String = #function, line: Int = #line, message: String) {
+    public class func emergency(file: String = #file, function: String = #function, line: Int = #line, message: String) {
         appendLog(type: .emergency, file: file, function: function, line: line, message: message)
     }
     
+
+    // MARK: - Private
+
     /// Append log action
     ///
     /// - Parameters:
@@ -47,38 +59,21 @@ class Log {
     ///   - message: log comment
     private class func appendLog(type: LogType, file: String, function: String, line: Int, message: String) {
         
-        let filePath = arrayBySplit(path: file, splitter: "/")
-        let txt: String = "\(getKRTime()) - \(type.rawValue), File: \(filePath.last!), Function: \(function)-\(line), Message: \(message)\n"
+        guard let loggingFileName = URL(string: file)?.lastPathComponent else {
+            print("Failed to found the file name.")
+            return
+        }
+        let txt = "\(getKRTime()) - \(type.rawValue), File: \(loggingFileName), Function: \(function)-\(line), Message: \(message)\n"
+
         getDirectory { (fileManager, directory) in
-            // Write log
-            let directory = directory + "/log.txt"
-            if fileManager.fileExists(atPath: directory) {
-                let readingLogFile = FileHandle(forReadingAtPath: directory)
-                if readingLogFile == nil {
-                    print("Log file open failed")
-                } else {
-                    writeLog(path: directory, log: txt)
-                }
-            } else {
-                if fileManager.createFile(atPath: directory, contents: nil, attributes: nil) {
-                    print("Log file create success")
-                    let readingLogFile = FileHandle(forReadingAtPath: directory)
-                    if readingLogFile == nil {
-                        print("Log file open failed")
-                    } else {
-                        writeLog(path: directory, log: txt)
-                    }
-                } else {
-                    print("Log file create failed")
-                }
-            }
+            writeLog(at: "\(directory)/\(fileName)", log: txt)
         }
     }
     
     /// Get Log file directory
     ///
     /// - Parameter complete: return filemanager, directory path
-    private class func getDirectory(complete: ((FileManager, String) -> (Void))? = nil) {
+    private class func getDirectory(complete: ((FileManager, String) -> (Void))) {
         
         // Set directory path
         let fileManager = FileManager.default
@@ -90,70 +85,78 @@ class Log {
         do {
             try fileManager.createDirectory(atPath: newDir, withIntermediateDirectories: true, attributes: nil)
         } catch let error as NSError {
-            NSLog("Error : \(error.localizedDescription)")
+            print("Error : \(error.localizedDescription)")
         }
-        complete!(fileManager, newDir)
+        complete(fileManager, newDir)
     }
     
     /// Get time to KR
     ///
     /// - Returns: "yyyy.MM.dd. hh.mm.ss"
     private class func getKRTime() -> String {
-        let now = NSDate()
+        let now = Date()
+
         let dateFormatter = DateFormatter()
-        dateFormatter.locale = NSLocale(localeIdentifier: "ko_KR") as Locale!
+        dateFormatter.locale = Locale(identifier: "ko_KR")
         dateFormatter.timeStyle = .medium
         dateFormatter.dateStyle = .medium
-        let nowdate = dateFormatter.string(from: now as Date)
-        return nowdate
+
+        return dateFormatter.string(from: now)
     }
     
     /// Write log
     ///
     /// - Parameters:
-    ///   - path: Log file path
+    ///   - at: Log file path
     ///   - log: Log text
-    private class func writeLog(path: String, log: String) {
-        let updateLogFile = FileHandle(forUpdatingAtPath: path)
-        updateLogFile?.seekToEndOfFile()
-        updateLogFile?.write(log.data(using: .utf8)!)
-        updateLogFile?.closeFile()
-        let splitLog = arrayBySplit(path: log, splitter: "/")
-        print(splitLog.last!)
+    private class func writeLog(at path: String, log: String) {
+        guard let data = log.data(using: .utf8) else {
+            print("Failed to convert string.")
+            return
+        }
+
+        guard let handle = openFile(at: path) else {
+            print("Log file open failed")
+            return
+        }
+        handle.seekToEndOfFile()
+        handle.write(data)
+        handle.closeFile()
+
+        let splitLog = log.components(separatedBy: "/")
+        print(splitLog.last ?? "")
+    }
+
+    /// Open the file, and get a FileHandle instance.
+    ///
+    /// - Parameters:
+    ///   - at: Log file path
+    private class func openFile(at path: String) -> FileHandle? {
+        let fileManager = FileManager.default
+
+        if fileManager.fileExists(atPath: path) {
+            return FileHandle(forUpdatingAtPath: path)
+        } else {
+            guard fileManager.createFile(atPath: path, contents: nil, attributes: nil) else {
+                print("Log file create failed")
+                return nil
+            }
+            print("Log file create success")
+
+            return FileHandle(forUpdatingAtPath: path)
+        }
     }
     
     /// Log file text clear
     private class func clear() {
         getDirectory { (fileManager, directory) in
-            let directory = directory + "/log.txt"
-            if fileManager.fileExists(atPath: directory) {
-                let readingLogFile = FileHandle(forReadingAtPath: directory)
-                if readingLogFile == nil {
-                    print("Log file open failed")
-                } else {
-                    let logFile = FileHandle(forUpdatingAtPath: directory)
-                    logFile?.truncateFile(atOffset: 0)
-                    logFile?.closeFile()
-                    print("Log file delete success")
-                }
-            } else {
-                print("Log file not exists")
+            guard let url = URL(string: "\(directory)/\(fileName)") else { return }
+
+            do {
+                try fileManager.removeItem(at: url)
+            } catch let error as NSError {
+                print("Error : \(error.localizedDescription)")
             }
         }
     }
-    
-    private class func arrayBySplit(path: String, splitter: String? = nil) -> [String] {
-        if let s = splitter {
-            return path.components(separatedBy: s)
-        } else {
-            return path.components(separatedBy: NSCharacterSet.whitespacesAndNewlines)
-        }
-    }
 }
-
-
-
-
-
-
-
